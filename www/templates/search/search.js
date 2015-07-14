ea9822c10af11890ffcdbcc175d0c65c2a6d4e05
Katy Justiss <katy.justiss@gmail.com>
2015-07-14 12:30:04 -0500

    worked on search capability

--- www/templates/search/search.js
+++ www/templates/search/search.js
@@ -1,4 +1,15 @@
 angular.module('weather.search', [])
-  .controller('SearchCtrl', function() {
-    console.log('test')
-  })
+  .controller('SearchCtrl', function($scope, $http) {
+    //randomly queryChange and query were on different scopes. Had to target the parent scope.
+    $scope.queryChanged = _.debounce(function() {
+      $http
+        .get('https://maps.googleapis.com/maps/api/geocode/json', {
+          params: { address: $scope.query}
+        })
+        .success(function (data) {
+          console.log(data);
+        });
+      }, 2000);
+    //display the cities. The user can click which one they want.
+
+  });
